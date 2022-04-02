1772fa0aa25dddd1b8809b3e583f4a8694ec3ddd
Elaine Nankanja <elaine.nankanja@dcmail.ca>
2022-04-02 01:56:44 -0400

    Added ICE11

---
+++ ICE11/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/contacts";
+export const RemoteURI = "mongodb+srv://thomas:k64TLy7vaCgsKWpb@cluster0.askzb.mongodb.net/contacts?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "WEBD6201SessionSecret";
